54e11181aa808e0531c1ca53acf48c6cd2808305
Himashasandeepani <135612092+Himashasandeepani@users.noreply.github.com>
2024-02-01 09:03:27 +0530

    Lab sheet 1

---
+++ Lab sheet 1/Lab sheet1(Q1)/Lab sheet1(Q1)/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_sheet_1_Q1_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length: ");
+            double length = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the width: ");
+            double width = double.Parse(Console.ReadLine());
+
+            double area = Area(length, width);
+
+            Console.WriteLine("The area of the rectangle is: " + area);
+            Console.ReadLine();
+        }
+        //we use static here becaouse we have used 2 parameters
+        static double Area(double length, double width)
+        {
+            return length * width;
+        }
+    }
+}
